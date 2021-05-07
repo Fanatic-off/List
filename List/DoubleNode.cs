8c11c2e713bfd0a61776c75f0e0bb89083d49433
Eugene <Zhekul.90@gmail.com>
2021-05-08 02:56:13 +0300

    add DoubleNode

---
+++ List/DoubleNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    class DoubleNode
+    {
+        public int Value { get; set; }
+        public DoubleNode Next { get; set; }
+        public DoubleNode Prev { get; set; }
+        public DoubleNode(int value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
